8554dd51ba6bdf9ef61f296aef1d7848e3706ba6
Erick Sosa Garcia <47539192+ericksosagarcia@users.noreply.github.com>
2019-08-24 18:12:46 -0400

    update

---
+++ test.js
@@ -0,0 +1,39 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        titulo: 'Homework app',
+        tareas: [],
+        nuevaTarea: ''
+    },
+    methods: {
+        addTarea(){
+            this.tareas.push({
+                nombre: this.nuevaTarea,
+                estado: false
+            })
+                this.nuevaTarea = ''
+                localStorage.setItem('homework-app', JSON.stringify(this.tareas))
+        },
+        remove(index){
+            this.tareas.pop()
+            localStorage.setItem('homework-app', JSON.stringify(this.tareas))
+        },
+
+        changeStade(index){
+            let a = this.tareas[index - 1]
+            a.estado = true
+            localStorage.setItem('homework-app', JSON.stringify(this.tareas))
+        }
+    },
+    created: function() {
+        let datosDB = JSON.parse(localStorage.getItem('homework-app'));
+        if (datosDB === null) {
+            this.tareas = [];
+        }
+        else{
+            this.tareas = datosDB;
+        }
+        console.log('App made with love by Erick')
+    }
+    
+})
